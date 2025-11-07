c5a09e70be31645d97d0bc42532f90b454096231
baldurt1992 <andresbaldur92@gmail.com>
2025-11-06 23:23:29 -0500

    Update favicon to 100x100 logo and change app name to Essential Innovation

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,6 +8,11 @@
 
     <title>{{ config('app.name', 'Essential Innovation') }}</title>
 
+    <!-- Favicon -->
+    <link rel="icon" type="image/jpeg" sizes="100x100" href="/images/cropped-logo-icon-web-100x100.jpg">
+    <link rel="shortcut icon" type="image/jpeg" href="/images/cropped-logo-icon-web-100x100.jpg">
+    <link rel="apple-touch-icon" sizes="100x100" href="/images/cropped-logo-icon-web-100x100.jpg">
+
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
